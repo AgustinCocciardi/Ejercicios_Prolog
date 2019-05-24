e49680723fd73d4e245f0a9a61d5ef7e6b720759
AgustinCocciardi <42738793+AgustinCocciardi@users.noreply.github.com>
2019-05-24 09:47:01 -0300

    Update funcion.pl

--- funcion.pl
+++ funcion.pl
@@ -14,6 +14,14 @@ fibonacci(0,0):-!.
 fibonacci(1,1):-!.
 fibonacci(X,Y):-X>1,X1 is X-1,X2 is X-2,fibonacci(X1,Y1),fibonacci(X2,Y2),Y is Y1+Y2.
 
+prod_suma_sucesiva(X,1,X):-!.
+prod_suma_sucesiva(X,Y,P):-Y>1,Y1 is Y-1,prod_suma_sucesiva(X,Y1,P1),P is P1+X.
 
+pow_prod_sucesiva(_,0,1):-!.
+pow_prod_sucesiva(X,1,X):-!.
+pow_prod_sucesiva(X,Y,P):-Y>1,Y1 is Y-1,pow_prod_sucesiva(X,Y1,P1), P is P1*X.
+
+div_resta_sucesiva(X,Y,0):-X<Y,!.
+div_resta_sucesiva(X,Y,P):-X>=Y,X1 is X-Y,div_resta_sucesiva(X1,Y,P1), P is P1+1.
 
 cls:-write('\e[2J').
