14fb4d1113f1a7132ea987b7df0751c65961721b
Agustin <aguscocciardi@gmail.com>
2019-05-31 22:16:49 -0300

    Actualizacion figura

--- area_figuras.pl
+++ area_figuras.pl
@@ -15,15 +15,17 @@ es_figura(cuadrado).
 es_figura(circulo).
 es_figura(rectangulo).
 
-area_triangulo(B,H,A):-es_figura(triangulo),triangulo(B,H), A is (B*H)/2.
-area_rectangulo(B,H,A):-es_figura(rectangulo),rectangulo(B,H), A is B*H.
-area_cuadrado(L,A):-es_figura(cuadrado),cuadrado(L), A is L*L.
-area_circulo(L,A):-es_figura(circulo),circulo(L), A is pi*L*L.
+area_triangulo(B,H,A):-triangulo(B,H), A is (B*H)/2.
+area_rectangulo(B,H,A):-rectangulo(B,H), A is B*H.
+area_cuadrado(L,A):-cuadrado(L), A is L*L.
+area_circulo(L,A):-circulo(L), A is pi*L*L.
 
-area_figura(FIG,A):-es_figura(FIG),dif(FIG,cuadrado),dif(FIG,circulo),dif(FIG,rectangulo),triangulo(B,H),area_triangulo(B,H,A).
-area_figura(FIG,A):-es_figura(FIG),dif(FIG,cuadrado),dif(FIG,circulo),dif(FIG,triangulo),rectangulo(B,H),area_rectangulo(B,H,A).
-area_figura(FIG,A):-es_figura(FIG),dif(FIG,triangulo),dif(FIG,circulo),dif(FIG,rectangulo),cuadrado(L),area_cuadrado(L,A).
-area_figura(FIG,A):-es_figura(FIG),dif(FIG,cuadrado),dif(FIG,triangulo),dif(FIG,rectangulo),circulo(L),area_circulo(L,A).
+es_distinto(X,Y,Z,W):-dif(X,Y),dif(X,Z),dif(X,W).
+
+area_figura(FIG,A):-es_figura(FIG),es_distinto(FIG,cuadrado,rectangulo,circulo),triangulo(B,H),area_triangulo(B,H,A).
+area_figura(FIG,A):-es_figura(FIG),es_distinto(FIG,cuadrado,triangulo,circulo),rectangulo(B,H),area_rectangulo(B,H,A).
+area_figura(FIG,A):-es_figura(FIG),es_distinto(FIG,triangulo,circulo,rectangulo),cuadrado(L),area_cuadrado(L,A).
+area_figura(FIG,A):-es_figura(FIG),es_distinto(FIG,cuadrado,rectangulo,triangulo),circulo(L),area_circulo(L,A).
 
 producto_cartesiano(F,A,G,B):-area_figura(F,A),area_figura(G,B).
 seleccion_menor(F,A,G,B):-producto_cartesiano(F,A,G,B), A<B.
